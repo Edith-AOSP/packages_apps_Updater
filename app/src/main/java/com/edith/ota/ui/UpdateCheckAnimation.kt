0e6f35dcf1ffa2ebdc15da0a298ad204af22e101
charcoalpt <thanhtrung08022010@gmail.com>
2026-08-09 14:53:20 +0700

    Updater: Pixel-fidelity UI refinements
    Change-Id: Id059331e04cf552e350b7794b5d300d72ace9ca3

--- app/src/main/java/com/edith/ota/ui/UpdateCheckAnimation.kt
+++ app/src/main/java/com/edith/ota/ui/UpdateCheckAnimation.kt
@@ -13,7 +13,7 @@ import com.android.settingslib.spa.debug.UiModePreviews
 import com.android.settingslib.spa.framework.theme.SettingsTheme
 import com.edith.ota.R
 
-private val AnimationSize = 260.dp
+private val AnimationSize = 320.dp
 
 @Composable
 fun UpdateCheckAnimation(modifier: Modifier = Modifier) {
