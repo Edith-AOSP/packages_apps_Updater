PixelOS-AOSP/android_packages_apps_Updater

Updater: Add certified props update support
Port the direct APK update flow from sixteen-qpr2 to the Compose preferences screen. Read the available version from the downloaded APK and install newer net.pixelos.certifiedprops.overlay builds through PackageInstaller.

Co-authored-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>
Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
Change-Id: I2fb1ad191fbd11ae3e022d0a0609e62c632dca4a

app/src/main/java/net/pixelos/ota/PackageInstallerStatusReceiver.kt
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2025 PixelOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.pixelos.ota
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInstaller
+
+class PackageInstallerStatusReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.getIntExtra(PackageInstaller.EXTRA_STATUS, -1)) {
+            PackageInstaller.STATUS_PENDING_USER_ACTION -> {
+                val confirmationIntent =
+                    intent.getParcelableExtra(Intent.EXTRA_INTENT, Intent::class.java)
+                confirmationIntent?.let {
+                    context.startActivity(it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+                }
+            }
+        }
+    }
+}

app/src/main/java/net/pixelos/ota/UpdaterApplication.kt
@@ -11,6 +11,7 @@ import com.android.settingslib.spa.framework.common.SpaEnvironment
 import com.android.settingslib.spa.framework.common.SpaEnvironmentFactory
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import net.pixelos.ota.certifiedprops.CertifiedPropsRepository
 import net.pixelos.ota.data.AppStateRepository
 import net.pixelos.ota.data.ChangelogRepository
 import net.pixelos.ota.data.UpdatesRepository
@@ -37,6 +38,7 @@ class UpdaterApplication : Application() {
     val notificationHelper by lazy { NotificationHelper(applicationContext) }
     val appStateRepository by lazy { AppStateRepository(applicationContext) }
     val changelogRepository by lazy { ChangelogRepository(applicationContext) }
+    val certifiedPropsRepository by lazy { CertifiedPropsRepository(applicationContext) }
     val userPreferencesRepository by lazy { UserPreferencesRepository(applicationContext) }
     val updatesRepository by lazy {
         UpdatesRepository(

app/src/main/java/net/pixelos/ota/download/APKDownloader.kt
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2025 PixelOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.pixelos.ota.download
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+
+class APKDownloader {
+    companion object {
+        private const val TIMEOUT_CONNECT = 15000
+        private const val TIMEOUT_READ = 30000
+        private const val BUFFER_SIZE = 262144
+        private const val TAG = "APKDownloader"
+
+        @JvmStatic
+        suspend fun downloadApk(path: String?, mUrl: String?): Boolean {
+            if (path.isNullOrEmpty() || mUrl.isNullOrEmpty()) {
+                return false
+            }
+
+            return withContext(Dispatchers.IO) {
+                try {
+                    val file = File(path).apply { if (exists()) delete() }
+                    val url = URL(mUrl)
+                    val urlConn = url.openConnection() as HttpURLConnection
+                    urlConn.apply {
+                        connectTimeout = TIMEOUT_CONNECT
+                        readTimeout = TIMEOUT_READ
+                        requestMethod = "GET"
+                        doInput = true
+                    }
+                    urlConn.connect()
+
+                    urlConn.inputStream.use { inputStream ->
+                        FileOutputStream(file).use { os ->
+                            val buffer = ByteArray(BUFFER_SIZE)
+                            var bytesRead: Int
+                            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                                os.write(buffer, 0, bytesRead)
+                            }
+                        }
+                    }
+                    Log.d(TAG, "Download successful: $path")
+                    true
+                } catch (e: SocketTimeoutException) {
+                    Log.e(TAG, "Download timed out", e)
+                    File(path).delete()
+                    false
+                } catch (e: Exception) {
+                    Log.e(TAG, "Download failed", e)
+                    File(path).delete()
+                    false
+                }
+            }
+        }
+    }
+}

app/src/main/java/net/pixelos/ota/preferences/PreferencesScreen.kt
@@ -18,18 +18,25 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.android.settingslib.spa.widget.preference.ListPreference
 import com.android.settingslib.spa.widget.preference.ListPreferenceModel
 import com.android.settingslib.spa.widget.preference.ListPreferenceOption
+import com.android.settingslib.spa.widget.preference.Preference
+import com.android.settingslib.spa.widget.preference.PreferenceModel
 import com.android.settingslib.spa.widget.preference.SwitchPreference
 import com.android.settingslib.spa.widget.preference.SwitchPreferenceModel
 import com.android.settingslib.spa.widget.scaffold.RegularScaffold
 import com.android.settingslib.spa.widget.ui.Category
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import net.pixelos.ota.R
 import net.pixelos.ota.UpdaterApplication
+import net.pixelos.ota.certifiedprops.CertifiedPropsError
+import net.pixelos.ota.certifiedprops.CertifiedPropsRepository
+import net.pixelos.ota.certifiedprops.CertifiedPropsState
 import net.pixelos.ota.data.CheckInterval
 import net.pixelos.ota.data.UserPreferencesRepository
 import net.pixelos.ota.deviceinfo.DeviceInfoUtils
 import net.pixelos.ota.util.BatteryMonitor
 import java.io.File
+import java.util.Locale
 
 @Composable
 fun PreferencesScreen() {
@@ -49,6 +56,7 @@ fun PreferencesScreen() {
         PreferencesContent(
             repository,
             batteryMonitor,
+            application.certifiedPropsRepository,
             isABDevice,
             supportsAbPerformanceMode,
             showRecoveryUpdate,
@@ -60,6 +68,7 @@ fun PreferencesScreen() {
 private fun PreferencesContent(
     repository: UserPreferencesRepository,
     batteryMonitor: BatteryMonitor,
+    certifiedPropsRepository: CertifiedPropsRepository,
     isABDevice: Boolean,
     supportsAbPerformanceMode: Boolean,
     showRecoveryUpdate: Boolean,
@@ -189,6 +198,82 @@ private fun PreferencesContent(
                 }
             })
         }
+    }
+
+    CertifiedPropsPreferences(certifiedPropsRepository, coroutineScope)
+}
+
+@Composable
+private fun CertifiedPropsPreferences(
+    repository: CertifiedPropsRepository,
+    coroutineScope: CoroutineScope,
+) {
+    val state by repository.state.collectAsStateWithLifecycle()
+    val unknown = stringResource(R.string.text_download_size_unknown)
+    val installedVersion = formatCertifiedPropsVersion(state.installedVersion, unknown)
+    val remoteVersion = when (val currentState = state) {
+        is CertifiedPropsState.UpToDate -> currentState.remoteVersion
+        is CertifiedPropsState.UpdateAvailable -> currentState.remoteVersion
+        is CertifiedPropsState.Installing -> currentState.remoteVersion
+        else -> -1
+    }
+    val versionSummary = stringResource(
+        R.string.certified_prop_info,
+        installedVersion,
+    ) + stringResource(
+        R.string.certified_prop_remote,
+        formatCertifiedPropsVersion(remoteVersion, unknown),
+    )
+    val summary = when (val currentState = state) {
+        is CertifiedPropsState.Checking -> stringResource(R.string.certified_prop_checking)
+        is CertifiedPropsState.Installing -> stringResource(R.string.certified_prop_downloading)
+        is CertifiedPropsState.Installed -> stringResource(R.string.certified_prop_install_success)
+        is CertifiedPropsState.Error -> when (currentState.error) {
+            CertifiedPropsError.DOWNLOAD_FAILED ->
+                stringResource(R.string.certified_prop_download_failed)
+            CertifiedPropsError.INSTALL_FAILED ->
+                stringResource(R.string.certified_prop_install_failed)
+            CertifiedPropsError.INVALID_PACKAGE ->
+                stringResource(R.string.certified_prop_invalid_package)
+        }
+        else -> versionSummary
+    }
+    val actionTitle = if (state is CertifiedPropsState.UpdateAvailable) {
+        stringResource(R.string.certified_prop_available)
+    } else {
+        stringResource(R.string.certified_prop_check)
+    }
+    val busy = state is CertifiedPropsState.Checking || state is CertifiedPropsState.Installing
+
+    Category(title = stringResource(R.string.play_integrity_header)) {
+        Preference(object : PreferenceModel {
+            override val title = stringResource(R.string.certified_prop_status)
+            override val summary = { summary }
+        })
+        Preference(object : PreferenceModel {
+            override val title = actionTitle
+            override val enabled = { !busy }
+            override val onClick: () -> Unit = {
+                coroutineScope.launch {
+                    if (state is CertifiedPropsState.UpdateAvailable) {
+                        repository.installUpdate()
+                    } else {
+                        repository.checkForUpdate()
+                    }
+                }
+            }
+        })
+    }
+}
+
+private fun formatCertifiedPropsVersion(version: Long, unknown: String): String {
+    return if (version > 0) {
+        val versionString = version.toString().padStart(3, '0')
+        val major = versionString.dropLast(2).toInt()
+        val minor = versionString.takeLast(2).toInt()
+        String.format(Locale.getDefault(), "%d.%02d", major, minor)
+    } else {
+        unknown
     }
 }
 

Edith-AOSP/packages_apps_Updater

app/src/main/java/com/edith/ota/PackageInstallerStatusReceiver.kt
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2025 PixelOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.edith.ota
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInstaller
+
+class PackageInstallerStatusReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.getIntExtra(PackageInstaller.EXTRA_STATUS, -1)) {
+            PackageInstaller.STATUS_PENDING_USER_ACTION -> {
+                val confirmationIntent =
+                    intent.getParcelableExtra(Intent.EXTRA_INTENT, Intent::class.java)
+                confirmationIntent?.let {
+                    context.startActivity(it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+                }
+            }
+        }
+    }
+}

app/src/main/java/com/edith/ota/UpdaterApplication.kt
@@ -11,6 +11,7 @@ import com.android.settingslib.spa.framework.common.SpaEnvironment
 import com.android.settingslib.spa.framework.common.SpaEnvironmentFactory
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import com.edith.ota.certifiedprops.CertifiedPropsRepository
 import com.edith.ota.data.AppStateRepository
 import com.edith.ota.data.ChangelogRepository
 import com.edith.ota.data.UpdatesRepository
@@ -37,6 +38,7 @@ class UpdaterApplication : Application() {
     val notificationHelper by lazy { NotificationHelper(applicationContext) }
     val appStateRepository by lazy { AppStateRepository(applicationContext) }
     val changelogRepository by lazy { ChangelogRepository(applicationContext) }
+    val certifiedPropsRepository by lazy { CertifiedPropsRepository(applicationContext) }
     val userPreferencesRepository by lazy { UserPreferencesRepository(applicationContext) }
     val updatesRepository by lazy {
         UpdatesRepository(

app/src/main/java/com/edith/ota/download/APKDownloader.kt
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2025 PixelOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.edith.ota.download
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+
+class APKDownloader {
+    companion object {
+        private const val TIMEOUT_CONNECT = 15000
+        private const val TIMEOUT_READ = 30000
+        private const val BUFFER_SIZE = 262144
+        private const val TAG = "APKDownloader"
+
+        @JvmStatic
+        suspend fun downloadApk(path: String?, mUrl: String?): Boolean {
+            if (path.isNullOrEmpty() || mUrl.isNullOrEmpty()) {
+                return false
+            }
+
+            return withContext(Dispatchers.IO) {
+                try {
+                    val file = File(path).apply { if (exists()) delete() }
+                    val url = URL(mUrl)
+                    val urlConn = url.openConnection() as HttpURLConnection
+                    urlConn.apply {
+                        connectTimeout = TIMEOUT_CONNECT
+                        readTimeout = TIMEOUT_READ
+                        requestMethod = "GET"
+                        doInput = true
+                    }
+                    urlConn.connect()
+
+                    urlConn.inputStream.use { inputStream ->
+                        FileOutputStream(file).use { os ->
+                            val buffer = ByteArray(BUFFER_SIZE)
+                            var bytesRead: Int
+                            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                                os.write(buffer, 0, bytesRead)
+                            }
+                        }
+                    }
+                    Log.d(TAG, "Download successful: $path")
+                    true
+                } catch (e: SocketTimeoutException) {
+                    Log.e(TAG, "Download timed out", e)
+                    File(path).delete()
+                    false
+                } catch (e: Exception) {
+                    Log.e(TAG, "Download failed", e)
+                    File(path).delete()
+                    false
+                }
+            }
+        }
+    }
+}

app/src/main/java/com/edith/ota/preferences/PreferencesScreen.kt
@@ -18,18 +18,25 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.android.settingslib.spa.widget.preference.ListPreference
 import com.android.settingslib.spa.widget.preference.ListPreferenceModel
 import com.android.settingslib.spa.widget.preference.ListPreferenceOption
+import com.android.settingslib.spa.widget.preference.Preference
+import com.android.settingslib.spa.widget.preference.PreferenceModel
 import com.android.settingslib.spa.widget.preference.SwitchPreference
 import com.android.settingslib.spa.widget.preference.SwitchPreferenceModel
 import com.android.settingslib.spa.widget.scaffold.RegularScaffold
 import com.android.settingslib.spa.widget.ui.Category
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import com.edith.ota.R
 import com.edith.ota.UpdaterApplication
+import com.edith.ota.certifiedprops.CertifiedPropsError
+import com.edith.ota.certifiedprops.CertifiedPropsRepository
+import com.edith.ota.certifiedprops.CertifiedPropsState
 import com.edith.ota.data.CheckInterval
 import com.edith.ota.data.UserPreferencesRepository
 import com.edith.ota.deviceinfo.DeviceInfoUtils
 import com.edith.ota.util.BatteryMonitor
 import java.io.File
+import java.util.Locale
 
 @Composable
 fun PreferencesScreen() {
@@ -49,6 +56,7 @@ fun PreferencesScreen() {
         PreferencesContent(
             repository,
             batteryMonitor,
+            application.certifiedPropsRepository,
             isABDevice,
             supportsAbPerformanceMode,
             showRecoveryUpdate,
@@ -60,6 +68,7 @@ fun PreferencesScreen() {
 private fun PreferencesContent(
     repository: UserPreferencesRepository,
     batteryMonitor: BatteryMonitor,
+    certifiedPropsRepository: CertifiedPropsRepository,
     isABDevice: Boolean,
     supportsAbPerformanceMode: Boolean,
     showRecoveryUpdate: Boolean,
@@ -189,6 +198,82 @@ private fun PreferencesContent(
                 }
             })
         }
+    }
+
+    CertifiedPropsPreferences(certifiedPropsRepository, coroutineScope)
+}
+
+@Composable
+private fun CertifiedPropsPreferences(
+    repository: CertifiedPropsRepository,
+    coroutineScope: CoroutineScope,
+) {
+    val state by repository.state.collectAsStateWithLifecycle()
+    val unknown = stringResource(R.string.text_download_size_unknown)
+    val installedVersion = formatCertifiedPropsVersion(state.installedVersion, unknown)
+    val remoteVersion = when (val currentState = state) {
+        is CertifiedPropsState.UpToDate -> currentState.remoteVersion
+        is CertifiedPropsState.UpdateAvailable -> currentState.remoteVersion
+        is CertifiedPropsState.Installing -> currentState.remoteVersion
+        else -> -1
+    }
+    val versionSummary = stringResource(
+        R.string.certified_prop_info,
+        installedVersion,
+    ) + stringResource(
+        R.string.certified_prop_remote,
+        formatCertifiedPropsVersion(remoteVersion, unknown),
+    )
+    val summary = when (val currentState = state) {
+        is CertifiedPropsState.Checking -> stringResource(R.string.certified_prop_checking)
+        is CertifiedPropsState.Installing -> stringResource(R.string.certified_prop_downloading)
+        is CertifiedPropsState.Installed -> stringResource(R.string.certified_prop_install_success)
+        is CertifiedPropsState.Error -> when (currentState.error) {
+            CertifiedPropsError.DOWNLOAD_FAILED ->
+                stringResource(R.string.certified_prop_download_failed)
+            CertifiedPropsError.INSTALL_FAILED ->
+                stringResource(R.string.certified_prop_install_failed)
+            CertifiedPropsError.INVALID_PACKAGE ->
+                stringResource(R.string.certified_prop_invalid_package)
+        }
+        else -> versionSummary
+    }
+    val actionTitle = if (state is CertifiedPropsState.UpdateAvailable) {
+        stringResource(R.string.certified_prop_available)
+    } else {
+        stringResource(R.string.certified_prop_check)
+    }
+    val busy = state is CertifiedPropsState.Checking || state is CertifiedPropsState.Installing
+
+    Category(title = stringResource(R.string.play_integrity_header)) {
+        Preference(object : PreferenceModel {
+            override val title = stringResource(R.string.certified_prop_status)
+            override val summary = { summary }
+        })
+        Preference(object : PreferenceModel {
+            override val title = actionTitle
+            override val enabled = { !busy }
+            override val onClick: () -> Unit = {
+                coroutineScope.launch {
+                    if (state is CertifiedPropsState.UpdateAvailable) {
+                        repository.installUpdate()
+                    } else {
+                        repository.checkForUpdate()
+                    }
+                }
+            }
+        })
+    }
+}
+
+private fun formatCertifiedPropsVersion(version: Long, unknown: String): String {
+    return if (version > 0) {
+        val versionString = version.toString().padStart(3, '0')
+        val major = versionString.dropLast(2).toInt()
+        val minor = versionString.takeLast(2).toInt()
+        String.format(Locale.getDefault(), "%d.%02d", major, minor)
+    } else {
+        unknown
     }
 }
 
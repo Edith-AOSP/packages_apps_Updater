3e1e741160f51f879fe80d66666fdf5705a103f4
Ashwin Devi Srinivasan <astroashwin@outlook.com>
2026-08-20 07:23:45 +0000

    Updater: Add certified props update support
    Port the direct APK update flow from sixteen-qpr2 to the Compose preferences screen. Read the available version from the downloaded APK and install newer net.pixelos.certifiedprops.overlay builds through PackageInstaller.
    
    Co-authored-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I2fb1ad191fbd11ae3e022d0a0609e62c632dca4a

--- app/src/main/java/com/edith/ota/UpdaterApplication.kt
+++ app/src/main/java/com/edith/ota/UpdaterApplication.kt
@@ -11,6 +11,7 @@ import com.android.settingslib.spa.framework.common.SpaEnvironment
 import com.android.settingslib.spa.framework.common.SpaEnvironmentFactory
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import com.edith.ota.certifiedprops.CertifiedPropsRepository
 import com.edith.ota.data.AppStateRepository
 import com.edith.ota.data.ChangelogRepository
 import com.edith.ota.data.UpdatesRepository
@@ -37,6 +38,7 @@ class UpdaterApplication : Application() {
     val notificationHelper by lazy { NotificationHelper(applicationContext) }
     val appStateRepository by lazy { AppStateRepository(applicationContext) }
     val changelogRepository by lazy { ChangelogRepository(applicationContext) }
+    val certifiedPropsRepository by lazy { CertifiedPropsRepository(applicationContext) }
     val userPreferencesRepository by lazy { UserPreferencesRepository(applicationContext) }
     val updatesRepository by lazy {
         UpdatesRepository(
